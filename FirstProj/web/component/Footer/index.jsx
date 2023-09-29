ece81368a29f9e64160f0b66e3a3b5db229ea9d7
Анударь Оюунбат <LP230018@G301-CC0132.local>
2023-09-29 18:29:22 +0800

    new lesson and other

--- FirstProj/web/component/Footer/index.jsx
+++ FirstProj/web/component/Footer/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="bg-[#F6F6F7]">Footer</div>;
-}
+  return (
+    <div className="bg-[#F6F6F7]">
+      <div>
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </p>
+        <h3>
+          Email :<span>nfo@jstemplate.net</span>
+        </h3>
+        <h3>
+          Phone :<span>880 123 456 789</span>
+        </h3>
+      </div>
+      <div>
+        <h2>Home</h2>
+        <h2>Blog</h2>
+        <h2>Contact</h2>
+      </div>
+      <div>
+        <img src="./face" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
