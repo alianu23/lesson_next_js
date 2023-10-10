26e1f87456000f2d61bfe1ee46fb7caa2efdc388
alianu23 <142651654+alianu23@users.noreply.github.com>
2023-10-10 17:04:14 +0800

    contact Page uurchlult

--- FirstProj/web/components/ContactSection/ContactMessage/index.jsx
+++ FirstProj/web/components/ContactSection/ContactMessage/index.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const ContactMessage = () => {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    try {
+      const response = await fetch("/api/contact", {
+        method: "post",
+        body: new URLSearchParams(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Invalid response: ${response.status}`);
+      }
+      alert("Thanks for contacting us, we will get back to you soon!");
+    } catch (err) {
+      console.error(err);
+      alert("We can't submit the form, try again later?");
+    }
+  }
+
   return (
     <div className="mt-11 w-auto pl-[35px] pr-[200px] pt-[29px] pb-[26px] bg-neutral-100 rounded-xl ">
       <div>
@@ -8,12 +26,19 @@ const ContactMessage = () => {
         <div>
           <input
             type="text"
+            id="frm-name"
+            autoComplete="given-name"
             placeholder="Your Name"
+            required
             className="border py-[14px] pr-[14px] pl-5 border-zinc-200 rounded mr-7"
           />
           <input
-            type="text"
+            type="email"
+            id="frm-email"
+            name="email"
+            autoComplete="email"
             placeholder="Your Email"
+            required
             className="border py-[14px] pr-[14px] pl-5 border-zinc-200 rounded"
           />
         </div>
@@ -23,16 +48,21 @@ const ContactMessage = () => {
           <input
             type="text"
             placeholder="Subject"
+            required
             className="border py-[14px] pr-[14px] pl-5 border-zinc-200 rounded mb-7"
           />
           <textarea
             placeholder="Write a Message"
             cols="30"
             rows="10"
+            id="frm-message"
             className="border py-[14px] pr-[14px] pl-5 border-zinc-200 rounded"
           ></textarea>
         </div>
-        <button className="bg-[#4B6BFB] py-2 px-4 text-white rounded-md mt-8 ">
+        <button
+          type="submit"
+          className="bg-[#4B6BFB] py-2 px-4 text-white rounded-md mt-8 "
+        >
           Send Message
         </button>
       </div>
